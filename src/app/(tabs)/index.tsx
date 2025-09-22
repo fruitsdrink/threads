c9449b6e793629e52f354be393cb0e017112b3f6
fruitsdrink <fruitsdrink@126.com>
2025-09-23 00:11:30 +0800

    navigation dark theme

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hello world!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Slot } from "expo-router";
 
+import {ThemeProvider, DarkTheme} from '@react-navigation/native'
+
+const myTheme: typeof DarkTheme = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        primary: '#e5e5e7',
+        background: '#212121',
+        card: '#212121',
+        border: '#212121',
+    }
+}
+
+
 export default function RootLayout(){
-    return <Slot />
+    return <ThemeProvider value={myTheme}>
+        <Slot />
+    </ThemeProvider>
 }
